4dc7c57ac59f18cfb7793a26a2de9897586120d8
Sivadhanush <sivadhanush726@gmail.com>
2025-08-19 17:29:07 -0700

    Initial commit

---
+++ src/j42/sri.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j42;
+
+/**
+ *
+ * @author A
+ */
+public class sri 
+{
+    public void add()
+    {
+        System.out.println("hiiiiiiiiiiiiiiiiiii");
+        
+    }
+    
+}
